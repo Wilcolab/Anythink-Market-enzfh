acf0227a72022e6200162386b47236cb1210dbcb
FeliΞ <moongod101@hotmail.com>
2022-09-12 08:53:00 +0000

    remove "reuqired" mark

--- backend/models/Item.js
+++ backend/models/Item.js
@@ -8,10 +8,7 @@ var ItemSchema = new mongoose.Schema(
     slug: { type: String, lowercase: true, unique: true },
     title: String,
     description: String,
-    image: {
-      type: String,
-      required: true,
-    },
+    image: String,
     favoritesCount: { type: Number, default: 0 },
     comments: [{ type: mongoose.Schema.Types.ObjectId, ref: "Comment" }],
     tagList: [{ type: String }],
